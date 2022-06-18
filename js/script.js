47a9f14a9593608df946f00be9235d59431c8230
viviana rodriguez <vivianaprodriguez@live.com>
2022-06-18 13:28:30 -0400

    corrected apikey

--- js/script.js
+++ js/script.js
@@ -1,22 +1,25 @@
-
 var searchButton = document.getElementById('search-button');
 var searchFormEl = document.querySelector('#search-form');
-var apiKey = `https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={4b46a7690a3185fb5de09f671e285e9a}`;
+// var apiKey = `https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=4b46a7690a3185fb5de09f671e285e9a`;
+// var apiKey = `https://api.openweathermap.org/data/2.5/weather?q=formSubmitHandler&appid=4b46a7690a3185fb5de09f671e285e9a&units=imperial`;
 
 var formSubmitHandler = function (event) {
   event.preventDefault();
 
-  if (cityName) {
-    getUpdatedWeather(cityName);
+  var cityName = 'calabasas'
+
+  // if (cityName) {
+  //   getUpdatedWeather(cityName);
 
     // clear old content
-    repoContainerEl.textContent = '';
-  } else {
-    alert('Please enter the name of a city.');
-  }
-};
+    // repoContainerEl.textContent = '';
+  // } else {
+  //   alert('Please enter the name of a city.');
+  // }
+
 // format the github api url
-var apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={4b46a7690a3185fb5de09f671e285e9a}`;
+// var apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=4b46a7690a3185fb5de09f671e285e9a`;
+var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=4b46a7690a3185fb5de09f671e285e9a&units=imperial";
 
 // make a get request to url
 fetch(apiUrl).then(function (response) {
@@ -29,6 +32,7 @@ fetch(apiUrl).then(function (response) {
     });
   }
 });
+};
 
-searchFormEl.addEventListener('submit', formSubmitHandler);
+searchFormEl.addEventListener('click', formSubmitHandler);
 
