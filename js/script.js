dde71bafd5710b593aed41a34d367edf45470780
viviana rodriguez <vivianaprodriguez@live.com>
2022-06-18 01:13:16 -0400

    added api key

--- js/script.js
+++ js/script.js
@@ -1,29 +1,34 @@
-var city = document.getElementById("searchBtn");
 
-var clickButton = function(event) {
-var cityName = city.val.trim();
-   if (cityName) {
-       getUpdatedWeather(cityName);
-    
-      // clear old content
-    repoContainerEl.textContent = ""; 
-   }
-}
+var searchButton = document.getElementById('search-button');
+var searchFormEl = document.querySelector('#search-form');
+var apiKey = `https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={4b46a7690a3185fb5de09f671e285e9a}`;
 
-var getUpdatedWeather = function(weather) {
-    // format the github api url
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={API key}";
+var formSubmitHandler = function (event) {
+  event.preventDefault();
 
-    // make a get request to url
-  fetch(apiUrl)
-   .then(function(response) {
-     // request was successful
-     if (response.ok){
-       console.log(response);
-       response.json().then(function(data) {
-        console.log(data);
-        // displayRepos(data, user);
-      });
-    }
- })
-}
+  if (cityName) {
+    getUpdatedWeather(cityName);
+
+    // clear old content
+    repoContainerEl.textContent = '';
+  } else {
+    alert('Please enter the name of a city.');
+  }
+};
+// format the github api url
+var apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={4b46a7690a3185fb5de09f671e285e9a}`;
+
+// make a get request to url
+fetch(apiUrl).then(function (response) {
+  // request was successful
+  if (response.ok) {
+    console.log(response);
+    response.json().then(function (data) {
+      console.log(data);
+      // displayRepos(data, user);
+    });
+  }
+});
+
+searchFormEl.addEventListener('submit', formSubmitHandler);
+
