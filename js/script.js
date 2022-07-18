af1394791fbf9abde6ec954211d0d687871a604d
viviana rodriguez <vivianaprodriguez@live.com>
2022-07-18 00:14:48 -0400

    added search history

--- js/script.js
+++ js/script.js
@@ -10,7 +10,8 @@ var formSubmitHandler = function (event) {
   var apiKey = "4b46a7690a3185fb5de09f671e285e9a";
 
 // format the github api url
-// var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=4b46a7690a3185fb5de09f671e285e9a&units=imperial";
+var apiCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&Appid=" + apiKey + "&units=imperial";
+
 var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey + "&units=imperial";
 
 // make a get request to url
@@ -21,7 +22,6 @@ fetch(apiUrl).then(function (response) {
     response.json().then(function (data) {
       console.log(data);
 
-    //  Day 1
       var list = data.list[0];
       console.log(list);
       var temp = data.list[0].main.temp;
@@ -32,18 +32,72 @@ fetch(apiUrl).then(function (response) {
       console.log(wind);
       var humidity = data.list[0].main.humidity;
       console.log(humidity);
-      // var main = data.main.temp;
-      // console.log(main);
-      // var humidity = data.main.humidity;
-      // console.log(humidity);
-      // var weatherDescriptions = data.weather[0].description;
-      // console.log(weatherDescriptions);
-      // var windSpeed = data.wind.speed;
-      // console.log(windSpeed);
     });
-  }
+
+    fetch(apiCurrent).then(function (response) {
+      // request was successful
+      if (response.ok) {
+        // console.log(response);
+        response.json().then(function (data) {
+          console.log(data);
+
+      var main = data.main.temp;
+      console.log(main);
+      var humidity = data.main.humidity;
+      console.log(humidity);
+      var weatherDescriptions = data.weather[0].description;
+      console.log(weatherDescriptions);
+      var windSpeed = data.wind.speed;
+      console.log(windSpeed);
+
+      var searchHistory = function(){
+        for(var i = 0; i < cities.length; i++){
+            var searchButton = $('<button value= "'+cities[i]+'">'+cities[i]+'</button>');
+            $("#search").append(searchButton);
+    
+        }
+    }
+
+   searchHistory();
+
+   $("form").on("click", "button", function(event) {
+     event.preventDefault();
+
+   cityName= $("input").val().trim();
+
+   if(cityName === ""){
+      alert("input is empty")        
+      return;
+    }
+   
+    else{
+     $("input").val('');
+    
+    var lowerCity = city.map(element => {
+        return element.toLowerCase();
+    });
+    var cityNameLow = cityName.toLowerCase();
+
+    if(lowerCity.includes(cityNameLow)){
+        alert("This city has been serched before")
+    }
+    else{
+       
+        getCityLocation(cityName);
+
+        var newButton = $('<button value= "'+cityName+'">'+ cityName +'</button>');
+        $("#search").append(newButton); 
+
+        saveSearch();
+     }
+    }
+   });
+  });
+ };
+});
+};
 });
 };
 
-searchFormEl.addEventListener('click', formSubmitHandler);
 
+searchFormEl.addEventListener('click', formSubmitHandler);
